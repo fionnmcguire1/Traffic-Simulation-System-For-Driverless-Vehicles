acf75482d5b533d985c35e75596f382afb9aad6f
fionnmcguire1 <fionnmcguire1@gmail.com>
2016-10-31 23:16:25 +0000

    Car moves on it's Own & stays on map
    Implemented move functions to make the car move on it’s own around the
    map. The car stays on the map permanently. This works based on the Lerp
    function and hardcoded co-ordinates

--- TSSDV 1.0/Assets/PlayerControls.js
+++ TSSDV 1.0/Assets/PlayerControls.js
@@ -4,9 +4,17 @@ var moveLeft : KeyCode;
 var moveRight : KeyCode;
 var moveUp : KeyCode;
 var moveDown : KeyCode;
+//var target : Transform;
+//var timeSinceStarted : float = Time.deltaTime;
 
 var speed : float = 10;
+var checker : boolean = true;
+//var coOrd;
 
+function Start () {
+
+//CallingAllFunctions();
+}
 
 function Update () {
  //if(GetComponent.<Rigidbody2D>().position.y == 8.5){}
@@ -17,29 +25,29 @@ function Update () {
 
  if (Input.GetKey(moveLeft))
  {
- //while(GetComponent.<Rigidbody2D>().position.x <= 4.37 &&  GetComponent.<Rigidbody2D>().position.x >= -4.37){
-  GetComponent.<Rigidbody2D>().velocity.x = -speed;
-
-  //}
+ GetComponent.<Rigidbody2D>().velocity.x = -speed;
 
  }
  else if (Input.GetKey(moveRight))
  {
-  
-  GetComponent.<Rigidbody2D>().velocity.x = speed;
-
-
+  /*Move (8.5,-4.37);
+  Move (8.5,-3.9);
+  Move (-7.9,3.9);
+  Move (7.9,4.37);*/
+  CallingAllFunctions ();
+ 
  }
  else if (Input.GetKey(moveUp))
  {
   GetComponent.<Rigidbody2D>().velocity.y = speed;
-  transform.Rotate (Vector3.forward *30);
+  //transform.Rotate (Vector3.forward *30);
 
  }
  else if (Input.GetKey(moveDown))
  {
   GetComponent.<Rigidbody2D>().velocity.y = -speed;
-  transform.Rotate (Vector3.back*30);
+  //transform.Rotate (Vector3.back*30);
+
 
  }
  else 
@@ -49,3 +57,37 @@ function Update () {
   
  }
 }
+
+ function CallingAllFunctions () {
+ 	StartCoroutine(Move(8.5,-4.37));
+ 	yield WaitForSeconds(4);
+    StartCoroutine(Move (8.5,3.9));
+    yield WaitForSeconds(4);
+    StartCoroutine(Move (-7.9,3.9));
+    yield WaitForSeconds(4);
+    StartCoroutine(Move (-7.9,-4.37));
+
+
+ }
+
+function Move (x,y) {
+    checker = true;
+    var timeSinceStarted : float = 0f;
+    var startingPosition = transform.position;
+    while (checker === true)
+    {
+
+        timeSinceStarted = timeSinceStarted + Time.deltaTime*0.5;
+        transform.position = Vector3.Lerp(startingPosition, Vector2(x,y), timeSinceStarted);
+
+        // If the object has arrived, stop the coroutine
+        if (transform.position ==  Vector2(x,y))
+        {
+            checker = false;
+        }
+
+        // Otherwise, continue next frame
+        yield WaitForSeconds (0.01);
+    }
+ }
+
