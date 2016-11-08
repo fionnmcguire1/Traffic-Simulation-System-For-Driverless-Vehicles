d389a91d9facc3934743a673dec28c1759962c9b
fionnmcguire1 <fionnmcguire1@gmail.com>
2016-11-08 03:39:24 +0000

    Prototype #2 New Map, 10 Cars with IDs
    Created prefab of car, assigned names to the cars which operate as IDs,
    all created in game setup. The instances of the car gameobject poll
    between 2 points repeatedly and instantiate on a random X coordinate on
    the 0y coordinate. Also implemented a new  printed map of a section of
    New York. Source: Google Maps.

---
+++ TSSDV 1.1/Assets/GameSetup.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+//Creates an array called AllSprites
+ public var AllSprites : Sprite[];
+ public var Cars : GameObject[];
+ public var car : GameObject;
+ //public var new_Cars : GameObject[];
+ var i =0;
+ var node_x_list = [-6.85,-6.2,-5.55,-4.9,-4.2,-3.55,-2.87,-2.15,-1.4,-0.75,-0.07,0.6,1.3,1.98,2.63,3.3,4,4.65,5.34,6];
+ var node_y_list = [2.35,0,2.63];
+
+ function Start () {
+
+	  //Imports all the sprites in the folder Car_Types in Resources
+      var imports : Object[] = Resources.LoadAll("Car_Types/" , Sprite);
+
+      //Adjusting the array size
+      AllSprites = new Sprite[imports.Length];
+
+      //For each element in the array the AllSprites element is set to the imports element.
+      for(i = 0 ; i < AllSprites.Length ; i++)
+      {      
+          //AllSprites[i] = imports[i];
+      }
+      Cars = GameObject.FindGameObjectsWithTag("Car");
+
+      var index = 1;
+      while(index < 11)
+      {  
+      	car = Instantiate(Cars[0], new Vector3(0,node_x_list[ Random.Range(0,19)], 0), Quaternion.identity) as GameObject;
+      	car.name = "Car"+index;
+      	//Cars[index] = car;
+      	//Debug.log("Got In HERE");
+      	//new_Cars[i].name = Cars[i]
+      	index=index+1;
+      	//Cars[i].GetComponent<SpriteRenderer>().sprite = (Sprite)AllSprites[i];
+      }
+     
+ }
---
+++ TSSDV 1.1/Assets/PlayerControls.js
@@ -0,0 +1,106 @@
+﻿#pragma strict
+
+/*
+Prototype: 1
+Author: Fionn Mcguire
+Date: 01/11/2016
+*/
+
+/*
+List Of New York Nodes
+X Values  -6.85,-6.2,-5.55,-4.9,-4.2,-3.55,-2.87,-2.15,-1.4,-0.75,-0.07,0.6,1.3,1.98,2.63,3.3,4,4.65,5.34,6
+Y Values 2.35,0,2.63
+*/
+
+//These variables are for user input
+var moveLeft : KeyCode;
+var moveRight : KeyCode;
+var moveUp : KeyCode;
+var moveDown : KeyCode;
+
+//This controled the speed of the car when responding to user input which changed veolocity by this much
+var speed : float = 3; //This would represent 30km/h however this must be tinkered with as it's still too fast.
+//Checker exits the while loop in the travel function which finishes the function.
+var checker : boolean = true;
+//Array of nodes i.e a route
+var node_list= new Array();
+//node_list = [8.5,-4.37,8.5,3.9,-7.9,3.9,-7.9,-4.37];
+node_list = [-1.4,0,3,0,-1.4,0,3,0,-1.4,0,3,0,-1.4,0,3,0];
+//This function executes everything at startup immediately
+function Start () {
+//rb = GetComponent.<Rigidbody>();
+Travel(node_list,0,4);
+}
+//When the user inputs anything the code to make a change goes in here
+function Update () {
+
+//If i want to run the code again i just hight the right arrow key
+ if (Input.GetKey(moveRight))
+ {
+  Travel(node_list,-7.9,-4.37);
+ }
+ else 
+ {
+ 	//This ensures the sprite doesn't move when nothing happens
+   GetComponent.<Rigidbody2D>().velocity.x = 0;
+   GetComponent.<Rigidbody2D>().velocity.y = 0;
+ }
+}
+
+/*The travel function is basically a route executer
+The function accepts in an array of nodes, a destination x and a destination y
+The node array should be made up of even indexs 0 2 4 6 8 etc which are X co-ordinates
+and odd indexs 1 3 5 7 9 which make up the Y co-ordinates.
+The function sets the checker to true, timeSinceStarted, StartingPosition to 0.
+timeSinceStarted is used to measure how long it has been since the beginning of the travel between 2 nodes.
+This is a fraction of a second (time it takes to complete a frame). I used the MoveForward function to travel from
+point A (startingPosition) to point B (destination Node). The "steps" are calculated by multiplying the time by speed
+of the vehicle. Every time the object moves closer, the while loop checks
+if the object is at it's final destination, or if the object is at it's node destination. If it's at it's node destination then 
+the i variabe is incremented to get the next set of co-ordinates to make the new destination node and go all over again. 
+If the current position of the object is the destination x,y then checker is set to false and the loop exists as does the 
+function. The yield at the bottom of the function is just to make it seem asthoug the car is constantly moving as opposed 
+to jumping from one posiition to the next.
+*/
+
+ function Travel (node_list : Array, destinationX, destinationY) 
+ {
+	checker = true;
+    var timeSinceStarted : float = 0f;
+    var startingPosition = transform.position;
+    var i =0;
+
+    while (checker === true)
+    {
+
+
+    	//if (OnCollisionEnter(gameObject) == false)
+    	//{
+	        timeSinceStarted = timeSinceStarted + Time.deltaTime*speed;
+	        transform.position = Vector3.MoveTowards(startingPosition, Vector2(node_list[i],node_list[i+1]), timeSinceStarted);
+	        transform.right = Vector2(node_list[i],node_list[i+1]) - transform.position;
+
+	        if (transform.position ==  Vector2(destinationX,destinationY))
+	        {
+	            checker = false;
+	        }
+	        else if(transform.position == Vector2(node_list[i],node_list[i+1]))
+	        {
+	        	startingPosition = transform.position;
+	        	timeSinceStarted = 0f;
+	        	i = i+2;
+	        }
+	        yield WaitForSeconds (0.01);
+        /*}
+        else{
+        	checker = false;
+        }*/
+    }
+ }
+ /*
+ //if the car gameobject is collided with another the collidER (not the victim) will be destroyed
+ function OnCollisionEnter2D( collider : Collision2D )
+ {
+     Destroy(collider.gameObject);
+ }
+ */
